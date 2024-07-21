61c101ffc8fbeebb90ba9a3df52b550bf62406e7
xxbher <117332669+xxbher@users.noreply.github.com>
2024-07-22 00:14:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,17 +13,26 @@ io.on("connection", (socket) => {
   const dataid = socket.handshake.query.dataid;
   socket.join(dataid); // Join the room based on dataid
 
-  app.get("/trigger-api/:dataid/:doorNumber", (req, res) => {
-    const dataid = req.params.dataid;
-    const doorNumber = req.params.doorNumber;
-  
-    // Include a response in the "api-triggered" event
-    const responseMessage = "API trigger message sent to clients in the room";
-  
-    // Emit the "api-triggered" event
-    io.to(dataid).emit("api-triggered", { doorNumber, response: responseMessage });
-  
-    // Listen for the client-response event and include it in res.send
+  // Event handlers for the socket connection can be added here
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ${socket.id}`);
+  });
+});
+
+// Define the API endpoint outside of the connection block
+app.get("/trigger-api/:dataid/:doorNumber", (req, res) => {
+  const dataid = req.params.dataid;
+  const doorNumber = req.params.doorNumber;
+
+  // Include a response in the "api-triggered" event
+  const responseMessage = "API trigger message sent to clients in the room";
+
+  // Emit the "api-triggered" event
+  io.to(dataid).emit("api-triggered", { doorNumber, response: responseMessage });
+
+  // Listen for the client-response event from any client in the room
+  io.of("/").adapter.rooms.get(dataid).forEach(socketId => {
+    const socket = io.sockets.sockets.get(socketId);
     socket.once("client-response", (clientResponse) => {
       res.send({
         apiResponse: "API request received",
@@ -31,16 +40,7 @@ io.on("connection", (socket) => {
       });
     });
   });
-
-
-
-
-
-
 });
-
-
-
 
 const PORT = process.env.PORT || 7777;
 server.listen(PORT, () => {
